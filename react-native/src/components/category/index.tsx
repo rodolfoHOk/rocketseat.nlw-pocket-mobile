22972922c863f817ad4ca2568dc204185d4723c6
Rodolfo HiOk <rodolfo.okino@gmail.com>
2024-12-10 16:57:50 -0300

    feat: react-native: add places list in home screen

--- react-native/src/components/category/index.tsx
+++ react-native/src/components/category/index.tsx
@@ -13,7 +13,10 @@ export function Category({ name, iconId, isSelected = false, ...rest }: Props) {
   const Icon = categoriesIcons[iconId];
 
   return (
-    <Pressable style={[s.container, isSelected && s.containerSelected]}>
+    <Pressable
+      style={[s.container, isSelected && s.containerSelected]}
+      {...rest}
+    >
       <Icon size={16} color={colors.gray[isSelected ? 100 : 400]} />
 
       <Text style={[s.name, isSelected && s.nameSelected]}>{name}</Text>
